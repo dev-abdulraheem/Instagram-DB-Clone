3a8caaa82b4c6d10ba17dc91f168103918f1e7cd
unknown <dev-abdulraheem>
2023-08-09 07:39:54 +0100

    Records Updated

--- ig_clone_tables.sql
+++ ig_clone_tables.sql
@@ -52,7 +52,7 @@ CREATE TABLE Follows (
 
 -- Create Tags Table
 CREATE TABLE tags (
-  tag_id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   tag_name VARCHAR(255) UNIQUE,
   created_at TIMESTAMP DEFAULT NOW()
 );
@@ -61,7 +61,53 @@ CREATE TABLE tags (
 CREATE TABLE photo_tags (
     photo_id INT NOT NULL,
     tag_id INT NOT NULL,
-    FOREIGN KEY(photo_id) REFERENCES photos(photo_id),
-    FOREIGN KEY(tag_id) REFERENCES tags(tag_id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    FOREIGN KEY(tag_id) REFERENCES tags(id),
     PRIMARY KEY(photo_id, tag_id)
 );
+-- Insert Records Into The Users Table
+INSERT INTO users(username)
+VALUES("BlueTheCat"),
+	  ("CharlieBrown"),
+      ("Coltesteele");
+
+-- Insert Records Into The Photos Table
+INSERT INTO Photos(image_url, user_id)
+VALUES("/alsg45f",1),
+	  ("/fdhr56f",2),
+      ("/kfhjfk34l",2);
+      
+-- Insert Records Into The Comments Table
+INSERT INTO Comments(comment_text, user_id, photo_id)
+VALUES("Meow",1,2),
+	  ("Amazing Shot",2,3),
+      ("I <3 This",2,1);
+      
+-- Insert Records Into The Likes Table
+INSERT INTO Likes(user_id, photo_id)
+VALUES(1,1),
+	  (2,1),
+      (1,2),
+      (1,3),
+      (3,3);
+      
+-- Insert Records Into The Likes Table
+INSERT INTO Follows(follower_id, followee_id)
+VALUES(1,2),
+	  (1,3),
+      (3,1),
+      (2,3);
+      
+-- Insert Records Into The Tags Table
+INSERT INTO Tags(tag_name)
+VALUES("Adorable"),
+	  ("Cute"),
+      ("Adorable"),
+      ("Sunrise");
+      
+-- Insert Records Into The Photo Tags Table
+INSERT INTO photo_tags(follower_id, followee_id)
+VALUES(1,1),
+	  (1,2),
+      (2,3),
+      (3,2);
